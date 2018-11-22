8f762da0a156a03f67f4146280079481c5574679
qiudong <qiu_dong@suixingpay.com>
2018-11-22 13:56:54 +0800

    1111

--- template-front/src/main/java/com/template/front/api/demo/UserController.java
+++ template-front/src/main/java/com/template/front/api/demo/UserController.java
@@ -4,6 +4,7 @@ import com.template.front.api.AbstractController;
 import com.template.front.api.common.ResponseMessage;
 import com.template.orm.mapper.TuserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +16,13 @@ import java.text.SimpleDateFormat;
  * @version: V1.0
  * @review: qiudong /2018/7/23
  */
+@RequestMapping("/api")
 @RestController
 public class UserController extends AbstractController {
     @Autowired
     private TuserMapper tuserMapper;
 
-    @RequestMapping("/")
+    @PostMapping("/login/account")
     public ResponseMessage home(){
         return wrapperSupplier(() -> {
             String date = "222112";
